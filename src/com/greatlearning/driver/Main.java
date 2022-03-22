2630fb6fcdc89f78f594ffcff1bf5b6d8e0ccd51
kripindas <kripindask@gmail.com>
2022-03-22 19:59:11 +0530

    OOPs assignment solution added

---
+++ src/com/greatlearning/driver/Main.java
@@ -0,0 +1,36 @@
+package com.greatlearning.driver;
+
+import com.greatlearning.entity.AdminDepartment;
+import com.greatlearning.entity.HrDepartment;
+import com.greatlearning.entity.TechDepartment;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		AdminDepartment adminDepartment = new AdminDepartment();
+		System.out.println("Welcome to" + adminDepartment.departmentName());
+		System.out.println(adminDepartment.getTodaysWork());
+		System.out.println(adminDepartment.getWorkDeadline());
+		System.out.println(adminDepartment.isTodayAHoliday());
+
+		System.out.println();
+
+		HrDepartment hrDepartment = new HrDepartment();
+		System.out.println("Welcome to" + hrDepartment.departmentName());
+		System.out.println(hrDepartment.doActivity());
+		System.out.println(hrDepartment.getTodaysWork());
+		System.out.println(hrDepartment.getWorkDeadline());
+		System.out.println(hrDepartment.isTodayAHoliday());
+
+		System.out.println();
+
+		TechDepartment techDepartment = new TechDepartment();
+		System.out.println("Welcome to" + techDepartment.departmentName());
+		System.out.println(techDepartment.getTodaysWork());
+		System.out.println(techDepartment.getWorkDeadline());
+		System.out.println(techDepartment.getTechStackInformation());
+		System.out.println(techDepartment.isTodayAHoliday());
+	}
+
+}
